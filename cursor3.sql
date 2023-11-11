3ad0121381fd0693c8b20c1d0ead6a3ede20198e
Althaf Nazeer <119403840+Althaf-Nazeer@users.noreply.github.com>
2023-11-11 23:00:31 +0530

    Create cursor3.sql

---
+++ cursor3.sql
@@ -0,0 +1,92 @@
+Employee Id:1
+Employee Name:Alice
+Salary :10000
+Hire Date:01-01-21
+Employee name:Alice is Grater that 5
+Years
+-----------------------------
+New Salary : 11000
+==============================
+Employee Id:2
+Employee Name:Bob
+Salary :20000
+Hire Date:01-01-20
+Employee name:Bob is Grater that 5
+Years
+-----------------------------
+New Salary : 21000
+==============================
+Employee Id:3
+Employee Name:Cindy
+Salary :30000
+Hire Date:01-01-19
+Employee name:Cindy is Grater that 5
+Years
+-----------------------------
+New Salary : 31000
+==============================
+Employee Id:4
+Employee Name:Sam
+Salary :40000
+Hire Date:01-01-18
+Employee name:Sam is Less that 5
+Years
+-----------------------------
+New Salary : 40500
+==============================
+Employee Id:5
+Employee Name:Eric
+Salary :50000
+Hire Date:01-01-17
+Employee name:Eric is Less that 5
+Years
+-----------------------------
+New Salary : 50500
+==============================
+Employee Id:6
+Employee Name:Tom
+Salary :60000
+Hire Date:01-01-16
+Employee name:Tom is Less that 5
+Years
+-----------------------------
+New Salary : 60500
+==============================
+Employee Id:7
+Employee Name:John
+Salary :70000
+Hire Date:01-01-15
+Employee name:John is Less that 5
+Years
+-----------------------------
+New Salary : 70500
+==============================
+Employee Id:8
+Employee Name:Sari
+Salary :80000
+Hire Date:01-01-14
+Employee name:Sari is Less that 5
+Years
+-----------------------------
+New Salary : 80500
+==============================
+Employee Id:9
+Employee Name:Timo
+Salary :90000
+Hire Date:01-01-13
+Employee name:Timo is Less that 5
+Years
+-----------------------------
+New Salary : 90500
+==============================
+Employee Id:10
+Employee Name:Skove
+Salary :100000
+Hire Date:01-01-12
+Employee name:Skove is Less that 5
+Years
+-----------------------------
+New Salary : 100500
+==============================
+
+PL/SQL procedure successfully completed.
