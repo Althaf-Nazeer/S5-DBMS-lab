8e34e02f3f2f3191d98a8f87dcfce8097c95820c
Althaf Nazeer <119403840+Althaf-Nazeer@users.noreply.github.com>
2023-11-11 23:00:02 +0530

    Create cursor2.sql

---
+++ cursor2.sql
@@ -0,0 +1,33 @@
+EMPLOYEE ID:10
+EMPLOYEE NAME:Skove
+SALARY:100000
+*********************
+EMPLOYEE ID:9
+EMPLOYEE NAME:Timo
+SALARY:90000
+*********************
+EMPLOYEE ID:8
+EMPLOYEE NAME:Sari
+SALARY:80000
+*********************
+EMPLOYEE ID:7
+EMPLOYEE NAME:John
+SALARY:70000
+*********************
+EMPLOYEE ID:6
+EMPLOYEE NAME:Tom
+SALARY:60000
+*********************
+TOTAL SALARY:400000
+
+PL/SQL procedure successfully completed.
+
+
+
+///////
+connect
+username : system
+password : password
+//eof /
+
+@cursor1
