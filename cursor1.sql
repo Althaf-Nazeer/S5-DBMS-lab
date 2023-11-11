b31be6c05c57a200857c716b71a0a0f37d2f6d22
Althaf Nazeer <119403840+Althaf-Nazeer@users.noreply.github.com>
2023-11-11 22:59:30 +0530

    Create cursor1.sql

---
+++ cursor1.sql
@@ -0,0 +1,62 @@
+Employee Id:1
+Employee Name:Alice
+Employee Age:20
+Salary :10000
+Hire Date:01-01-21
+==============================
+Employee Id:2
+Employee Name:Bob
+Employee Age:30
+Salary :20000
+Hire Date:01-01-20
+==============================
+Employee Id:3
+Employee Name:Cindy
+Employee Age:40
+Salary :30000
+Hire Date:01-01-19
+==============================
+Employee Id:4
+Employee Name:Sam
+Employee Age:50
+Salary :40000
+Hire Date:01-01-18
+==============================
+Employee Id:5
+Employee Name:Eric
+Employee Age:60
+Salary :50000
+Hire Date:01-01-17
+==============================
+Employee Id:6
+Employee Name:Tom
+Employee Age:20
+Salary :60000
+Hire Date:01-01-16
+==============================
+Employee Id:7
+Employee Name:John
+Employee Age:30
+Salary :70000
+Hire Date:01-01-15
+==============================
+Employee Id:8
+Employee Name:Sari
+Employee Age:40
+Salary :80000
+Hire Date:01-01-14
+==============================
+Employee Id:9
+Employee Name:Timo
+Employee Age:50
+Salary :90000
+Hire Date:01-01-13
+==============================
+Employee Id:10
+Employee Name:Skove
+Employee Age:60
+Salary :100000
+Hire Date:01-01-12
+==============================
+
+PL/SQL procedure successfully completed.
